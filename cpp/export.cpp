b560121b15fdb57c8831a2e6af89512664279a63
daquexian <daquexian566@gmail.com>
2019-07-10 10:07:19 +0800

    Add erorr msg support, add output model format chosen, polish ui (add icons, colors, etc)

--- cpp/export.cpp
+++ cpp/export.cpp
@@ -12,10 +12,47 @@
 #include "onnx2ncnn.h"
 
 struct WasmBuffer {
-  unsigned char *output_buffer1;
-  unsigned char *output_buffer2;
-  size_t output_buffer_size1;
-  size_t output_buffer_size2;
+  unsigned char *output_buffer1 = nullptr;
+  unsigned char *output_buffer2 = nullptr;
+  size_t output_buffer_size1 = 0;
+  size_t output_buffer_size2 = 0;
+
+  void freeBuffers() {
+    freeBuffer1();
+    freeBuffer2();
+  }
+  void freeBuffer1() {
+    if (output_buffer1 != nullptr) {
+      free(output_buffer1);
+      output_buffer1 = nullptr;
+    }
+  }
+  void freeBuffer2() {
+    if (output_buffer2 != nullptr) {
+      free(output_buffer2);
+      output_buffer2 = nullptr;
+    }
+  }
+  void setBuffer1(const std::vector<char> &vec) {
+    output_buffer1 = static_cast<unsigned char *>(malloc(vec.size()));
+    memcpy(output_buffer1, vec.data(), vec.size());
+    output_buffer_size1 = vec.size();
+  }
+  void setBuffer2(const std::vector<char> &vec) {
+    output_buffer2 = static_cast<unsigned char *>(malloc(vec.size()));
+    memcpy(output_buffer2, vec.data(), vec.size());
+    output_buffer_size2 = vec.size();
+  }
+  void setBuffer1(const std::string &str) {
+    output_buffer1 = static_cast<unsigned char *>(malloc(str.size()));
+    memcpy(output_buffer1, str.c_str(), str.size());
+    output_buffer_size1 = str.size();
+  }
+  void setBuffer2(const std::string &str) {
+    output_buffer2 = static_cast<unsigned char *>(malloc(str.size()));
+    memcpy(output_buffer2, str.c_str(), str.size());
+    output_buffer_size2 = str.size();
+  }
 };
 
 extern "C" {
@@ -30,14 +67,7 @@ WasmBuffer *create_exporter() {
 
 void free_exporter(WasmBuffer *ctx) {
   if (ctx != NULL) {
-    if (ctx->output_buffer1 != NULL) {
-      free(ctx->output_buffer1);
-      ctx->output_buffer1 = NULL;
-    }
-    if (ctx->output_buffer2 != NULL) {
-      free(ctx->output_buffer2);
-      ctx->output_buffer2 = NULL;
-    }
+    ctx->freeBuffers();
     free(ctx);
     ctx = NULL;
   }
@@ -51,27 +81,26 @@ unsigned char *get_buffer2(WasmBuffer *ctx) { return ctx->output_buffer2; }
 
 size_t get_buffer_size2(WasmBuffer *ctx) { return ctx->output_buffer_size2; }
 
-void onnx2ncnn_export(WasmBuffer *ctx, const unsigned char *buffer,
+bool onnx2ncnn_export(WasmBuffer *ctx, const unsigned char *buffer,
                       const size_t bufferlen) {
   std::string buf_str(reinterpret_cast<const char *>(buffer), bufferlen);
   const auto expected_res = onnx2ncnn(buf_str);
   if (!expected_res) {
     std::cout << expected_res.error() << std::endl;
-    return;
+    ctx->setBuffer1(expected_res.error());
+    return false;
   }
   const auto res = expected_res.value();
   const auto pv = res.first;
   const auto bv = res.second;
   PNT(pv.size(), bv.size());
-  ctx->output_buffer1 = static_cast<unsigned char *>(malloc(pv.size()));
-  memcpy(ctx->output_buffer1, pv.data(), pv.size());
-  ctx->output_buffer_size1 = pv.size();
-  ctx->output_buffer2 = static_cast<unsigned char *>(malloc(bv.size()));
-  memcpy(ctx->output_buffer2, bv.data(), bv.size());
-  ctx->output_buffer_size2 = bv.size();
+  ctx->setBuffer1(pv);
+  ctx->setBuffer2(bv);
+
+  return true;
 }
 
-void caffe2ncnn_export(WasmBuffer *ctx, const unsigned char *prototxt_buffer,
+bool caffe2ncnn_export(WasmBuffer *ctx, const unsigned char *prototxt_buffer,
                        const size_t prototxt_bufferlen,
                        const unsigned char *caffemodel_buffer,
                        const size_t caffemodel_bufferlen) {
@@ -82,17 +111,15 @@ void caffe2ncnn_export(WasmBuffer *ctx, const unsigned char *prototxt_buffer,
   const auto expected_res = caffe2ncnn(prototxt_str, caffemodel_str);
   if (!expected_res) {
     std::cout << expected_res.error() << std::endl;
-    return;
+    ctx->setBuffer1(expected_res.error());
+    return false;
   }
   const auto res = expected_res.value();
   const auto pv = res.first;
   const auto bv = res.second;
   PNT(pv.size(), bv.size());
-  ctx->output_buffer1 = static_cast<unsigned char *>(malloc(pv.size()));
-  memcpy(ctx->output_buffer1, pv.data(), pv.size());
-  ctx->output_buffer_size1 = pv.size();
-  ctx->output_buffer2 = static_cast<unsigned char *>(malloc(bv.size()));
-  memcpy(ctx->output_buffer2, bv.data(), bv.size());
-  ctx->output_buffer_size2 = bv.size();
+  ctx->setBuffer1(pv);
+  ctx->setBuffer2(bv);
+  return true;
 }
 }
