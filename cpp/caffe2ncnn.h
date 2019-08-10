13663bf7b50d9d8b956c03e5604ca9871d991063
daquexian <daquexian566@gmail.com>
2019-08-10 14:52:04 +0800

    Manage ncnn in submodules and find protoc by find_program

--- cpp/caffe2ncnn.h
+++
@@ -1,4 +0,0 @@
-#include <expected.hpp>
-#include <wmc_utils.h>
-
-tl::expected<NcnnModel, std::string> caffe2ncnn(const std::string &prototxt_str, const std::string &model_str);
--- cpp/onnx2ncnn.h
+++
@@ -1,5 +0,0 @@
-#include <expected.hpp>
-#include <wmc_utils.h>
-
-tl::expected<NcnnModel, std::string> onnx2ncnn(const std::string &model_str);
-
