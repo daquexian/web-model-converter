8c1d40fd18a8a2e55f70f55e8388cff05ab2e11a
daquexian <daquexian566@gmail.com>
2020-08-04 19:21:43 +0800

    Add README.md

---
+++ README.md
@@ -0,0 +1,10 @@
+1. install emsdk
+2. run `source emsdk_env.sh`
+3. mkdir build && cd build
+4. run `emcmake cmake ..`
+5. run `emmake make export -j50`
+6. run `../upload_ali.sh`
+
+## update mnn
+1. cd path/to/mnn
+2. ./schema/generate.sh
---
+++ upload_ali.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+pushd build4
+gzip -c -9 export.wasm > export_gz.wasm
+ossutil64 --config-file ~/.ossutilconfig cp -u export_gz.wasm  oss://converter-web/export.wasm --meta=Content-Type:application/wasm#Content-Encoding:gzip
+ossutil64 --config-file ~/.ossutilconfig cp -u export.js oss://converter-web/
+popd
+pushd web/
+ossutil64 --config-file ~/.ossutilconfig cp -u index.html oss://converter-web/
+ossutil64 --config-file ~/.ossutilconfig cp -u robots.txt oss://converter-web/
+popd
